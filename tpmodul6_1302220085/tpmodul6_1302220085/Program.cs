487bbe324120d103803429c318a7ebced552795d
myousefg <myousefg@gmail.com>
2024-03-24 23:51:12 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302220085/tpmodul6_1302220085/Program.cs
+++ tpmodul6_1302220085/tpmodul6_1302220085/Program.cs
@@ -2,6 +2,32 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Yousef");
+        video.PrintVideoDetails();
+    }
+}
+
+class SayaTubeVideo {
+    int id;
+    string title;
+    int playCount;
+
+    public SayaTubeVideo(string title) 
+    {
+        this.title = title;
+        Random randomId = new Random();
+        id = randomId.Next(10000, 99999);
+    }
+
+    public void IncreasePlayCount(int increment)
+    {
+        playCount += increment;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID: " + id);
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Play count: " + playCount);
     }
 }
