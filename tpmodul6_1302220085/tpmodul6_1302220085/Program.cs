b7e110114bc14da8ef4c94a7aadb27886372b0b7
myousefg <myousefg@gmail.com>
2024-03-25 02:20:48 +0700

    implementasi design by contract

--- tpmodul6_1302220085/tpmodul6_1302220085/Program.cs
+++ tpmodul6_1302220085/tpmodul6_1302220085/Program.cs
@@ -2,8 +2,22 @@
 {
     private static void Main(string[] args)
     {
-        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Yousef");
-        video.PrintVideoDetails();
+        SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract – Yousef");
+        video1.PrintVideoDetails();
+        
+        SayaTubeVideo video2 = new SayaTubeVideo("");
+        video2.PrintVideoDetails();
+
+        SayaTubeVideo video3 = new SayaTubeVideo("Mengecek penambahan playcounter lebih dari 10.000.000 dan juga title yang lebih dari seratus karakter");
+        video3.IncreasePlayCount(100000001);
+        video3.PrintVideoDetails();
+
+        SayaTubeVideo video4 = new SayaTubeVideo("Mengecek playcount overflow");
+        for (int i = 0; i < 2; i++)
+        {
+            video4.IncreasePlayCount(1000000000);
+        }
+        video4.PrintVideoDetails();
     }
 }
 
@@ -17,11 +31,53 @@ class SayaTubeVideo {
         this.title = title;
         Random randomId = new Random();
         id = randomId.Next(10000, 99999);
+
+        try
+        {
+            this.title = title;
+
+            if (title == "")
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (title.Length > 100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("Title tidak boleh kosong");
+        }
+
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Title harus kurang dari 100 karakter");
+        }
     }
 
     public void IncreasePlayCount(int increment)
     {
-        playCount += increment;
+        try
+        {
+            checked
+            {
+                playCount += increment;
+                if (playCount + increment > 10000000) throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Play count tidak boleh lebih dari 10 juta");
+        }
+
+        catch (OverflowException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 
     public void PrintVideoDetails()
@@ -29,5 +85,6 @@ class SayaTubeVideo {
         Console.WriteLine("ID: " + id);
         Console.WriteLine("Title: " + title);
         Console.WriteLine("Play count: " + playCount);
+        Console.WriteLine("");
     }
 }
